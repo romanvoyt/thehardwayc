26116a9cdea098e7c7368edfb54c19e4c80790ee
Roman <romanvoitetckii@gmail.com>
2021-08-12 19:11:10 +0500

    added a comment to 1.c

--- ex1/1.c
+++ ex1/1.c
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 	char buffer[100];
 	puts("hello world.");
 	
+	//reading 100 char input from stdin
 	fread(buffer, sizeof(char), 100, stdin);
 	
 	printf("String: %s \n", buffer);
