8634a8b8e92ac22b3a339bf0752dca612a1e8353
Roman <romanvoitetckii@gmail.com>
2021-08-18 03:04:27 +0500

    New folder is added

---
+++ ex3/ex4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* this program has bugs*/
+
+int main()
+{
+	int age = 20;
+	int height;
+	
+	printf("I am %d years old. \n");
+	printf("I an %d inches tall. ]n", height);
+	
+	return 0;
+}
