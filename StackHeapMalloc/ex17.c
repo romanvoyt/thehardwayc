324f1b143144cb04cbc9260826f38f054c569e95
Roman <romanvoitetckii@gmail.com>
2021-08-19 12:08:09 +0500

    Main part of ex17 has been finished. Database was created.

--- StackHeapMalloc/ex17.c
+++ StackHeapMalloc/ex17.c
@@ -15,6 +15,10 @@ struct Address {
 };
 
 struct Database{
+	struct Address rows[MAX_ROWS];
+};
+
+struct Connection {
 	FILE *file;
 	struct Database *db;
 };
@@ -91,7 +95,99 @@ void Database_create(struct Connection *conn){
 
 void Database_set(struct Connection *conn, int id, const char *name, const char *email){
 	struct Address *addr = &conn->db->rows[id];
+	
+	if(addr->set) die("Already set, delete it first");
+	
+	addr->set = 1;
+	char *res = strncpy(addr->name, name, MAX_DATA);
+	
+	if(!res) die("Name copy failed");
+	
+	res = strncpy(addr->email, email, MAX_DATA);
+	if(!res) die("Email copy failed");
 }
+
+void Database_get(struct Connection *conn, int id){
+	struct Address *addr = &conn->db->rows[id];
+	
+	if(addr->set){
+		Address_print(addr);
+	} else {
+		die("ID is not set");
+	}
+}
+
+void Database_delete(struct Connection *conn, int id)
+{
+	struct Address addr = {.id = id, .set = 0};
+	conn->db->rows[id] = addr;
+}
+
+void Database_list(struct Connection *conn)
+{
+	int i = 0;
+	struct Database *db = conn->db;
+	
+	for(i = 0; i < MAX_ROWS; i++)
+	{
+		struct Address *cur = &db->rows[i];
+		
+		if(cur->set){
+			Address_print(cur);
+		}	
+	}
+}
+
+int main(int argc, char *argv[]){
+	if(argc < 3) die("Usage: ex17 <dbfile> <action> [action params]");
+	
+	char *filename = argv[1];
+	char action = argv[2][0];
+	struct Connection *conn = Database_open(filename, action);
+	int id = 0;
+	
+	if(argc > 3) id = atoi(argv[3]);
+	if(id >= MAX_ROWS) die("There's not that many records");
+	
+	switch(action){
+		case 'c':
+			Database_create(conn);
+			Database_write(conn);
+			break;
+		case 'g':
+			if(argc!=4) die("Need an id to get");
+			
+			Database_get(conn, id);
+			break;
+		case 's':
+			if(argc!=6) die("Need id, name, email to set");
+			
+			Database_set(conn, id, argv[4], argv[5]);
+			Database_write(conn);
+			break;
+		case 'd':
+			if(argc!=4) die("Need id to delete");
+			
+			Database_delete(conn, id);
+			Database_write(conn);
+			break;
+		case 'l':
+			Database_list(conn);
+			break;
+		default:
+			die("Invalid action, only: c=create, g=get, s=set, d=del, l=list");
+		
+	}
+	
+	Database_close(conn);
+	
+	return 0;
+}
+
+
+
+
+
 
 
 
